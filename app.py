588b3d507ef152fab3b878a0cb5bdf3ca1bea4ce
Mayo Olojo <mayoolojo@Mayos-MBP.fios-router.home>
2021-01-08 19:04:13 -0500

    set up linter

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+""" proper file """
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
+    """ more words """
     return "My flask app"
-  
+
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug=True)
